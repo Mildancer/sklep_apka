ff14b0d6c26670c1b2e168dc75bd6d6997e5a12b
milosz nawrat <milosznawrat@gmail.com>
2019-02-02 15:40:06 +0100

    przed refactoryzacja produktu

--- src/model/Alkohol.java
+++ src/model/Alkohol.java
@@ -4,38 +4,21 @@ import java.math.BigDecimal;
 
 public class Alkohol extends Produkt {
 
-   private static int generator = 1; // dla id by kazdy byl inny
 
-    private int id;                 // private by nie mogli zmieniac nam tych cen np w innych klasach
-    private  String nazwa;
-    private BigDecimal cena;
+
     private String pojemnosc;
     private  String procentAlkoholu;
 
     // kontruktor parametry ze wszystkimi parametrami (oprocz id)
 
     public Alkohol(String nazwa, BigDecimal cena, String pojemnosc, String procentAlkoholu) {
-        this.id = generator; // lub zapisac to po prostu tak:   this.id = generator;
-        // static- optymalizacja dla stalej, by pojawiala sie tylko raz/statyczna - zmienna 1  dla wszystkich obiektow
-        generator++;
-        this.nazwa = nazwa;
-        this.cena = cena;
+        // konstruktor klasy bazowej z ktorej dziedzinymy (Produkt-z parametrami produktu)
+
+        super(nazwa, cena);
         this.pojemnosc = pojemnosc;
         this.procentAlkoholu = procentAlkoholu;
     }
 
-    public int getId(){
-        return id;
-    }
-
-
-    @Override                      // kompilator wyswietli blad, gdy zrobimy literowke- poinformuje nas podczas kompilacji
-    public String toString(){                 // konwersja obiektu na string
-        return
-        getId() + "\t" + this.nazwa + "\t"+ this.cena + " PLN";                  // this z tej klasy
-
-
-    }
 
 
 }
--- src/model/Papierosy.java
+++ src/model/Papierosy.java
@@ -2,13 +2,11 @@ package model;
 
 import java.math.BigDecimal;
 
-public class Papierosy {
+public class Papierosy extends Produkt {
 
-   private static int generator = 1; // dla id by kazdy byl inny
 
-    private int id;                 // private by nie mogli zmieniac nam tych cen np w innych klasach
-    private  String nazwa;
-    private BigDecimal cena;
+
+
     private int ilosc;
     private String rodzaj;
 
@@ -16,27 +14,12 @@ public class Papierosy {
     // kontruktor parametry ze wszystkimi parametrami (oprocz id)
 
     public Papierosy(String nazwa, BigDecimal cena, int ilosc, String rodzaj) {
-        this.id = generator; // lub zapisac to po prostu tak:   this.id = generator;
-        // static- optymalizacja dla stalej, by pojawiala sie tylko raz/statyczna - zmienna 1  dla wszystkich obiektow
-        generator++;
-        this.nazwa = nazwa;
-        this.cena = cena;
+        super(nazwa, cena);
         this.ilosc = ilosc;
         this.rodzaj = rodzaj;
     }
 
-    public int getId(){
 
-        return id;
-    }
-
-    @Override                      // kompilator wyswietli blad, gdy zrobimy literowke- poinformuje nas podczas kompilacji
-    public String toString(){                 // konwersja obiektu na string
-        return
-        getId() + "\t" + this.nazwa + "\t"+ this.cena + " PLN";                  // this z tej klasy
-
-
-    }
 
 
 }
